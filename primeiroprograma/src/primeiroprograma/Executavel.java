e4dbf79050f22e369beb0eb7078264d6e48407b6
aluno <aluno@192.168.0.205>
2024-11-27 16:37:58 -0300

    segundo commit

--- primeiroprograma/src/primeiroprograma/Executavel.java
+++ primeiroprograma/src/primeiroprograma/Executavel.java
@@ -6,8 +6,16 @@ public class Executavel {
 		System.out.println("olá mundo!");
 		System.out.println("meu nome é THEO SANNA ALVARENGA");
 		
+		int inteiro;
+		double numeroDecimal;
+		String texto;
 		
-
+		//atribuição de valores as variáveis
+		inteiro = 5;
+		numeroDecimal = 5.65;
+		texto = "theo";
+		System.out.println("Nome: " + inteiro);
+		System.out.println("Numero decimal: " + numeroDecimal);
 	}
 
 }
