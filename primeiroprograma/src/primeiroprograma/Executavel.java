e6eaa63fa86d247cb38eedb301b709aee1785ad0
aluno <aluno@192.168.0.205>
2024-11-27 13:49:11 -0300

    Meu primeiro commit

---
+++ primeiroprograma/src/primeiroprograma/Executavel.java
@@ -0,0 +1,13 @@
+package primeiroprograma;
+
+public class Executavel {
+
+	public static void main(String[] args) {
+		System.out.println("olá mundo!");
+		System.out.println("meu nome é THEO SANNA ALVARENGA");
+		
+		
+
+	}
+
+}
